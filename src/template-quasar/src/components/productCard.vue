85d893f681d85ce3ce7a0edf885ad7281d5b5d2f
Idriss Sajide <idrisssajide@gmail.com>
2021-02-18 11:12:44 +0100

    ajout du template de site, c'est un projet quasar, tout marche

---
+++ src/template-quasar/src/components/productCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <q-card
+    @click="$router.push('/details')"
+    class="cursor-pointer"
+    :class="class_val"
+    @mouseover="class_val = 'my-card shadow-20'"
+    @mouseleave="class_val = 'my-card shadow-'"
+  >
+    <q-img src="https://www.famous-cbd.fr/214-large_default/cbd-amnesia-haze.jpg"></q-img>
+
+    <q-card-section class="q-pb-xs q-pt-md">
+      <!--<q-btn-->
+      <!--fab-->
+      <!--color="primary"-->
+      <!--icon="shopping_cart"-->
+      <!--class="absolute"-->
+      <!--style="top: 0; right: 12px; transform: translateY(-50%);"-->
+      <!--&gt;</q-btn>-->
+
+      <div class="row no-wrap items-center">
+        <div class="col text-subtitle2 ellipsis-2-lines text-grey-10">
+          Benling C200-BLK Smartwatch (Black Strap Free Size)
+        </div>
+        <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+          <!--<q-icon name="place"></q-icon>-->
+          <!--250 ft-->
+        </div>
+      </div>
+
+      <q-rating v-model="stars" color="orange" :max="5" readonly size="17px"></q-rating>
+    </q-card-section>
+
+    <q-card-section class="q-py-sm">
+      <div>
+        <div class="text-caption text-green-8 text-weight-bolder">Special Price</div>
+        <span class="text-h6">₹3,149</span
+        ><span class="q-ml-sm text-grey-6" style="text-decoration: line-through"
+          >₹3,699</span
+        >
+        <span class="q-ml-md text-caption text-green-8 text-weight-bolder q-mt-md"
+          >20% off</span
+        >
+      </div>
+      <!--<div class="text-subtitle1">-->
+      <!--$・Italian, Cafe-->
+      <!--</div>-->
+      <!--<div class="text-caption text-grey">-->
+      <!--Small plates, salads & sandwiches in an intimate setting.-->
+      <!--</div>-->
+    </q-card-section>
+
+    <q-separator></q-separator>
+
+    <q-card-actions>
+      <q-btn flat class="text-weight-bold text-capitalize" dense color="primary">
+        View details
+      </q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+
+<script>
+export default {
+  name: "category.vue",
+  data() {
+    return {
+      stars: 4,
+      class_val: "shadow-1 my-card",
+    };
+  },
+  computed: {
+    win_width() {
+      return this.$q.screen.width - 59;
+    },
+    win_height() {
+      return this.$q.screen.height - 0;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 300px;
+}
+</style>
---
+++ src/template-quasar/src/router/routes.js
@@ -0,0 +1,39 @@
+const routes = [
+  { path: "/", component: () => import("pages/login.vue") },
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        name: "home",
+        path: "/home",
+        component: () => import("pages/home.vue")
+      },
+      {
+        name: "customer_management",
+        path: "/customer_management",
+        component: () => import("pages/customer_management.vue")
+      },
+      {
+        name: "my_profile",
+        path: "/my_profile",
+        component: () => import("pages/my_profile.vue")
+      },
+      {
+        name: "sorties",
+        path: "/sorties",
+        component: () => import("pages/sorties.vue")
+      }
+    ]
+  }
+];
+
+// Always leave this as last one
+if (process.env.MODE !== "ssr") {
+  routes.push({
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
+}
+
+export default routes;
