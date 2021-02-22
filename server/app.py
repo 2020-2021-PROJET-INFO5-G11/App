f8ee5e3e159a4b3cd94a6c4f1deaf9af0539f81a
Rim El Jraidi <reljraidi@gmail.com>
2021-02-22 17:07:36 +0100

    [WIP] update api with groupes and commentaires fields

--- server/app.py
+++ server/app.py
@@ -70,6 +70,27 @@ SORTIES = [
     }
 ]
 
+GROUPES = [
+    {
+        'id': uuid.uuid4().hex,
+        'nom': 'Popo',
+        'membres': '',
+        'créateur': '',
+        'activités': '',
+        'description': 'test',
+    },
+]
+
+COMMENTAIRES = [
+    {
+        'id': uuid.uuid4().hex,
+        'utilisateur': 'Rim',
+        'contenu': 'Ceci est un commentaire',
+        'date': '',
+        'réponses': '',
+    },
+]
+
 
 # configuration
 DEBUG = True
@@ -86,6 +107,22 @@ def remove_sortie(sortie_id):
     for sortie in SORTIES:
         if sortie['id'] == sortie_id:
             SORTIES.remove(sortie)
+            return True
+    return False
+
+
+def remove_groupe(groupe_id):
+    for groupe in GROUPES:
+        if groupe['id'] == groupe_id:
+            GROUPES.remove(groupe)
+            return True
+    return False
+
+
+def remove_commentaire(commentaire_id):
+    for commentaire in COMMENTAIRES:
+        if commentaire['id'] == commentaire_id:
+            COMMENTAIRES.remove(commentaire)
             return True
     return False
 
@@ -122,6 +159,43 @@ def all_sorties():
     return jsonify(response_object)
 
 
+@app.route('/groupes', methods=['GET', 'POST'])
+def all_groupes():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        GROUPES.append({
+            'id': uuid.uuid4().hex,
+            'nom': post_data.get('nom'),
+            'membress': post_data.get('membres'),
+            'créateur': post_data.get('créateur'),
+            'activités': post_data.get('activités'),
+            'description': post_data.get('description'),
+        })
+        response_object['message'] = 'Groupe créé!'
+    else:
+        response_object['groupes'] = GROUPES
+    return jsonify(response_object)
+
+
+@app.route('/commentaires', methods=['GET', 'POST'])
+def all_commentaires():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        COMMENTAIRES.append({
+            'id': uuid.uuid4().hex,
+            'utilisateur': post_data.get('utilisateur'),
+            'contenu': post_data.get('contenu'),
+            'date': post_data.get('date'),
+            'réponses': post_data.get('réponses'),
+        })
+        response_object['message'] = 'Commentaire ajouté!'
+    else:
+        response_object['commentaires'] = COMMENTAIRES
+    return jsonify(response_object)
+
+
 @app.route('/sorties/<sortie_id>', methods=['PUT', 'DELETE'])
 def single_sortie(sortie_id):
     response_object = {'status': 'success'}
@@ -155,6 +229,46 @@ def single_sortie(sortie_id):
         response_object['message'] = 'Sortie supprimée!'
     return jsonify(response_object)
 
+@app.route('/groupes/<groupe_id>', methods=['PUT', 'DELETE'])
+def single_groupe(groupe_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_groupe(groupe_id)
+        GROUPES.append({
+            'id': uuid.uuid4().hex,
+            'nom': post_data.get('nom'),
+            'membress': post_data.get('membres'),
+            'créateur': post_data.get('créateur'),
+            'activités': post_data.get('activités'),
+            'description': post_data.get('description'),
+        })
+        response_object['message'] = 'Groupe mise à jour!'
+    if request.method == 'DELETE':
+        remove_groupe(groupe_id)
+        response_object['message'] = 'Groupe supprimé!'
+    return jsonify(response_object)
+
+
+@app.route('/commentaires/<commentaire_id>', methods=['PUT', 'DELETE'])
+def single_commentaire(commentaire_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_commentaire(commentaire_id)
+        COMMENTAIRES.append({
+            'id': uuid.uuid4().hex,
+            'nom': post_data.get('nom'),
+            'membress': post_data.get('membres'),
+            'créateur': post_data.get('créateur'),
+            'activités': post_data.get('activités'),
+            'description': post_data.get('description'),
+        })
+        response_object['message'] = 'Commentaire mise à jour!'
+    if request.method == 'DELETE':
+        remove_commentaire(commentaire_id)
+        response_object['message'] = 'Commentaire supprimé!'
+    return jsonify(response_object)
 
 if __name__ == '__main__':
     app.run()
