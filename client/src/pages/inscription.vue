37949016c4182b5bd521b104d0b2d1b023473ae6
Idriss Sajide <idrisssajide@gmail.com>
2021-02-23 12:55:47 +0100

    page d'inscription, champs email et nom

--- client/src/pages/inscription.vue
+++ client/src/pages/inscription.vue
@@ -1,6 +1,9 @@
 <template>
-<div>
+
+<div class="container" >
+    
    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+
     <b-form-group
         id="input-group-1"
         label="Email address:"
@@ -11,32 +14,25 @@
           id="input-1"
           v-model="form.email"
           type="email"
-          placeholder="Enter email"
+          placeholder="Adresse email"
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+      <b-form-group id="input-group-2" label="Nom:" label-for="input-2">
         <b-form-input
           id="input-2"
           v-model="form.name"
-          placeholder="Enter name"
+          placeholder="Entez votre nom"
           required
         ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          v-model="form.checked"
-          id="checkboxes-4"
-          :aria-describedby="ariaDescribedby"
-        >
-        </b-form-checkbox-group>
       </b-form-group>
 
       <b-button type="submit" variant="primary">S'inscrire</b-button>
       <b-button type="reset" variant="danger">Réinitialiser</b-button>
     </b-form>
+    
+
 </div>
 </template>
 
