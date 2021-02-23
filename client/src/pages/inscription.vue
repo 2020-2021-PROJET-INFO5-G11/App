b9493f0965f9ce63f337fcb821743c2d2a0fd0df
Idriss Sajide <idrisssajide@gmail.com>
2021-02-23 14:22:43 +0100

    register page with password needs to be improved

--- client/src/pages/inscription.vue
+++ client/src/pages/inscription.vue
@@ -2,14 +2,10 @@
 
 <div class="container" >
     
-   <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+   <b-form @submit="onSubmit" @reset="onReset" >
 
     <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
+        id="input-group-1" label="Email address:" label-for="input-1" description="Le mail ne sera pas partagé aux autres utilisateurs">
         <b-form-input
           id="input-1"
           v-model="form.email"
@@ -19,15 +15,48 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Nom:" label-for="input-2">
+        <b-form @submit.stop.prevent>
+    <label for="text-password">Password</label>
+    <b-form-input type="password" id="text-password" aria-describedby="password-help-block"></b-form-input>
+    <b-form-text id="password-help-block">
+      Your password must be 8-20 characters long, contain letters and numbers, and must not
+      contain spaces, special characters, or emoji.
+    </b-form-text>
+   </b-form>
+
+      <b-form-group id="input-group-2" label="Mot de passe:" label-for="input-2">
         <b-form-input
           id="input-2"
+          type="password"
+          v-model="form.password"
+          placeholder="Entez votre mot de passe"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Répétez votre mot de passe:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          type="passwordFieldType"
+          v-model="form.rePassword"
+          placeholder="Répétez votre mot de passe"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="password" @click="switchVisibility">show / hide</b-button>
+      
+
+      <b-form-group id="input-group-4" label="Nom:" label-for="input-4" description="Ce nom sera visible par les autres utilisateurs">
+        <b-form-input
+          id="input-4"
           v-model="form.name"
           placeholder="Entez votre nom"
           required
         ></b-form-input>
       </b-form-group>
 
+      
       <b-button type="submit" variant="primary">S'inscrire</b-button>
       <b-button type="reset" variant="danger">Réinitialiser</b-button>
     </b-form>
@@ -42,11 +71,11 @@
       return {
         form: {
           email: '',
+          password: '',
+          rePassword: '',
+          passwordFieldType: 'password',
           name: '',
-          food: null,
-          checked: []
         },
-        show: true
       }
     },
     methods: {
@@ -59,12 +88,19 @@
         // Reset our form values
         this.form.email = ''
         this.form.name = ''
+        this.form.password = ''
         // Trick to reset/clear native browser form validation state
         this.show = false
         this.$nextTick(() => {
           this.show = true
         })
-      }
+      },
+      passwordConfirmationRule() {
+      return () => (this.password === this.rePassword) || 'Password must match'
+    },
+      switchVisibility() {
+        this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password'
+      },
     }
   }
 </script>
