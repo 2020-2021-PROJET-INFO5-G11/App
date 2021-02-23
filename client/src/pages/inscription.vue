c4330e4287b5c84f6282d041446aea8c05daeb44
Idriss Sajide <idrisssajide@gmail.com>
2021-02-23 14:31:53 +0100

    ajout du camouflage du mot de passe

--- client/src/pages/inscription.vue
+++ client/src/pages/inscription.vue
@@ -15,16 +15,7 @@
         ></b-form-input>
       </b-form-group>
 
-        <b-form @submit.stop.prevent>
-    <label for="text-password">Password</label>
-    <b-form-input type="password" id="text-password" aria-describedby="password-help-block"></b-form-input>
-    <b-form-text id="password-help-block">
-      Your password must be 8-20 characters long, contain letters and numbers, and must not
-      contain spaces, special characters, or emoji.
-    </b-form-text>
-   </b-form>
-
-      <b-form-group id="input-group-2" label="Mot de passe:" label-for="input-2">
+      <b-form-group id="input-group-2" label="Mot de passe:" label-for="input-2" aria-describedby="password-help-block">
         <b-form-input
           id="input-2"
           type="password"
@@ -32,12 +23,15 @@
           placeholder="Entez votre mot de passe"
           required
         ></b-form-input>
+        <b-form-text id="password-help-block">
+      Votre mot de passe doit contenir bla bla bla
+    </b-form-text>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Répétez votre mot de passe:" label-for="input-3">
+      <b-form-group id="input-group-3" label="Répétez votre mot de passe:" label-for="input-3" aria-describedby="password-help-block">
         <b-form-input
           id="input-3"
-          type="passwordFieldType"
+          type="password"
           v-model="form.rePassword"
           placeholder="Répétez votre mot de passe"
           required
