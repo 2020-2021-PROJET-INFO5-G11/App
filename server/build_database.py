1ddd1a2bd15715cdc3b638a16cd9c78b6aeb1781
GabrielManiss <gabriel.manissadjian@etu.univ-grenoble-alpes.fr>
2021-02-25 16:26:14 +0100

    [WIP]

--- server/build_database.py
+++ server/build_database.py
@@ -3,10 +3,65 @@ from config import db
 from models import Sortie
 
 # Data to initialize database with
-SORTIE = [
-    {'nomSortie': 'Doug', 'typeSortie': 'Farrell'},
-    {'nomSortie': 'Kent', 'typeSortie': 'Brockman'},
-    {'nomSortie': 'Bunny','typeSortie': 'Easter'}
+SORTIES = [
+    {
+        'id_sortie': 1,
+        'nom': 'Rando entre copains',
+        'location': 'Monteynard',
+        'date': '',
+        'heure': '',
+        'duree': '',
+        'point_rdv': 'Polytech',
+        'capaciteMin': '',
+        'capaciteMax': '',
+        'privee': True,
+        'id_groupe': '',
+        'typeSortie': 'Sport',
+        'photo': '',
+        'nbInscrits': '',
+        'description': '',
+        'dateLimite': '',
+        'commentaires': ''
+    },
+    {
+        'id_sortie': 2,
+        'nom': 'Harry Potter and the Philosopher\'s Stone',
+        'location': 'Pathé Chavant',
+        'date': '',
+        'heure': '',
+        'duree': '',
+        'point_rdv': 'Polytech',
+        'capaciteMin': '',
+        'capaciteMax': '',
+        'privee': False,
+        'id_groupe': '',
+        'typeSortie': 'Cinéma',
+        'photo': '',
+        'nbInscrits': '',
+        'description': '',
+        'dateLimite': '',
+        'commentaires': ''
+
+    },
+    {
+        'id_sortie': 3,
+        'nom': 'Balade au PPM',
+        'location': 'Parc Paul Mistral',
+        'date': '',
+        'heure': '',
+        'duree': '',
+        'point_rdv': 'Polytech',
+        'capaciteMin': '',
+        'capaciteMax': '',
+        'privee': True,
+        'id_groupe': '',
+        'typeSortie': 'Autre',
+        'photo': '',
+        'nbInscrits': '',
+        'description': '',
+        'dateLimite': '',
+        'commentaires': ''
+    }
 ]
 
 # Delete database file if it exists currently
@@ -17,8 +72,9 @@ if os.path.exists('entities.db'):
 db.create_all()
 
 # Iterate over the SORTIE structure and populate the database
-for sort in SORTIE:
-    s = Sortie(nomSortie=sort['nomSortie'], typeSortie=sort['typeSortie'])
+for sort in SORTIES:
+    s = Sortie(nom=sort['nom'], typeSortie=sort['typeSortie'])
+    #s = sort.Sortie()
     db.session.add(s)
 
 db.session.commit()
--- server/models.py
+++ server/models.py
@@ -10,14 +10,14 @@ class User(db.Model):
     lastName = db.Column(db.String(32))
     """email = db.Column(db.String(32))
     photo = db.Column(db.String(32))
-    birthDate = db.Column(db.String(32))
-    city = db.Column(db.String(32))
+    dateNaissance = db.Column(db.String(32))
+    ville = db.Column(db.String(32))
     preferences = db.Column(db.String(32))
-    gender = db.Column(db.String(32))
+    sexe = db.Column(db.String(32))
     bio = db.Column(db.String(32))
-    activitiesIncoming = db.Column(db.String(32))
-    activitiesDone = db.Column(db.String(32))
-    activitiesOrganized = db.Column(db.String(32))
+    activites_a_venir = db.Column(db.String(32))
+    activites_finies = db.Column(db.String(32))
+    activites_organisees = db.Column(db.String(32))
     role = db.Column(db.String(32))
     feedbacks = db.Column(db.String(32))"""
 
@@ -30,8 +30,27 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
 class Sortie(db.Model):
     __tablename__ = 'sorties'
     id_sortie = db.Column(db.Integer, primary_key=True)
-    nomSortie = db.Column(db.String(32), index=True)
+    nom = db.Column(db.String(32), index=True)
+    location = db.Column(db.String(32))
+    date = db.Column(db.String(32))
+    heure = db.Column(db.String(32))
+    duree = db.Column(db.String(32))
+    point_rdv = db.Column(db.String(32))
+    capaciteMin = db.Column(db.Integer)
+    capacaiteMax = db.Column(db.Integer)
+    privee = db.Column(db.Boolean)
+    id_groupe = db.Column(db.Integer)
     typeSortie = db.Column(db.String(32))
+    photo = db.Column(db.String(32))
+    nbInscrits = db.Column(db.Integer)
+    description = db.Column(db.String(1024))
+    dateLimite = db.Column(db.String(32))
+    commentaires = db.Column(db.String(32))
+
+
+
+
+
     # timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 class SortieSchema(ma.SQLAlchemyAutoSchema):
