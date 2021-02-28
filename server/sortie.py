d74e72b04d481de052c750a8e3d1d04582e3f216
Rim El Jraidi <reljraidi@gmail.com>
2021-02-28 15:26:49 +0100

    Create sortie request created, need to test it

--- server/sortie.py
+++ server/sortie.py
@@ -25,3 +25,37 @@ def read_one_sortie_by_id(id):
         return sortie_schema.dump(sortie)
     else:
         abort(404, f'Sortie not found for id: {id}')
+
+def create(sortie):
+    """
+    This function creates a new sortie
+    based on the passed-in sortie data
+
+    :param sortie:    sortie to create
+    :return:        201 on success, 406 on sortie exists
+    """
+
+    id = sortie.get('id_sortie')
+    if Sortie.query.get(id) is not None:
+        abort(409, f'id {id} is already used')
+
+    nom = sortie.get('nom')
+    typeSortie = sortie.get('typeSortie')
+
+    existing_sortie = Sortie.query \
+        .filter(Sortie.nom == nom) \
+        .filter(Sortie.typeSortie == typeSortie) \
+        .one_or_none()
+
+    if existing_sortie is None:
+        schema = SortieSchema()
+        new_sortie = schema.load(sortie, session=db.session)
+
+        db.session.add(new_sortie)
+        db.session.commit()
+
+        return schema.dump(new_sortie), 201
+
+    else:
+        abort(409, f'Sortie {nom} {typeSortie} exists already')
+
