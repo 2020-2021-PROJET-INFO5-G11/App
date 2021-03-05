757e9d52744ee25d5bad58aabb201f3e466b26ea
Rim El Jraidi <reljraidi@gmail.com>
2021-03-05 10:54:12 +0100

    possibility to get comments of one sortie by its id

--- server/entities/commentaire.py
+++ server/entities/commentaire.py
@@ -13,6 +13,12 @@ def get_all():
     com_schema = ComSchema(many=True)
     return com_schema.dump(coms)
 
+def get_activity_comments(id_sortie):
+    coms = Commentaire.query.filter(Commentaire.id_sortie == id_sortie).order_by(db.desc(Commentaire.timestamp)).all()
+
+    com_schema = ComSchema(many=True)
+    return com_schema.dump(coms)
+
 
 def create(com):
     id = com.get('id_commentaire')
@@ -26,4 +32,46 @@ def create(com):
     db.session.commit()
 
     return schema.dump(new_com), 201
-    
+
+
+def update(id_commentaire, commentaire):
+    update_commentaire = Commentaire.query.filter(
+        Commentaire.id_commentaire == id_commentaire
+    ).one_or_none()
+
+    if update_commentaire is None:
+        abort(
+            404,
+            f'Commentaire not found for Id: {id_commentaire}',
+        )
+
+    else:
+
+        schema = ComSchema()
+        update = schema.load(commentaire, session=db.session)
+
+        update.id_commentaire = update_commentaire.id_commentaire
+
+        db.session.merge(update)
+        db.session.commit()
+
+        data = schema.dump(update_commentaire)
+
+        return data, 200
+
+
+def delete(id_commentaire):
+    commentaire = Commentaire.query.filter(Commentaire.id_commentaire == id_commentaire).one_or_none()
+
+    if commentaire is not None:
+        db.session.delete(commentaire)
+        db.session.commit()
+        return make_response(
+            "Commentaire {id_commentaire} deleted".format(id_commentaire=id_commentaire), 200
+        )
+
+    else:
+        abort(
+            404,
+            "Commentaire not found for Id: {id_commentaire}".format(id_commentaire=id_commentaire),
+        )
