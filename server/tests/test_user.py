334cf116607582af2b1ac73952f0fc1d11e715a2
Rim El Jraidi <reljraidi@gmail.com>
2021-03-16 11:33:17 +0100

    test get non existing user and update ok

--- server/tests/test_user.py
+++ server/tests/test_user.py
@@ -2,12 +2,51 @@ import unittest
 import requests
 
 class userTest(unittest.TestCase):
+    API_URL = "http://localhost:5000/api"
+    USERS_URL = "{}/user".format(API_URL)
+    USER_OBJ = {
+                "bio": "test_bio",
+                "dateNaissance": "12-03-1998",
+                "email": "test@gmail.com",
+                "feedbacks": "",
+                "nom": "test_nom",
+                "password_hash": "test_mdp",
+                "photo": "",
+                "preferences": "Sport, Culture",
+                "prenom": "test_prenom",
+                "role": "",
+                "sexe": "Femme",
+                "ville": "Grenoble"
+            }
+
+    def _get_each_user_url(self, id):
+        return "{}/{}".format(userTest.USERS_URL, id)
 
     # POST request to /api/user to create a new user
     def test_1_post_single_user(self):
-        r = requests.post("http://localhost:5000/api/user", json = 
+        r = requests.post(userTest.USERS_URL, json = userTest.USER_OBJ)
+        self.assertEqual(r.status_code, 201)
+
+    # GET request to /api/user returns the details of all users
+    def test_2_get_all_users(self):
+        r = requests.get(userTest.USERS_URL)
+        self.assertEqual(r.status_code, 200)
+
+    # GET request to /api/user returns the details of a single user
+    def test_3_get_single_user(self):
+        r = requests.get(self._get_each_user_url(1))
+        self.assertEqual(r.status_code, 200)
+
+    # GET request to /api/user trying to get non existing user --> shouldn't work
+    def test_4_get_non_existing_user(self):
+        r = requests.get(self._get_each_user_url(509))
+        self.assertEqual(r.status_code, 404)
+
+    # PUT request to /api/user to update a user
+    def test_5_update_user(self):
+        r = requests.put(self._get_each_user_url(2), json = 
             {
-                "bio": "test_bio",
+                "bio": "test_update",
                 "dateNaissance": "12-03-1998",
                 "email": "test@gmail.com",
                 "feedbacks": "",
@@ -22,13 +61,3 @@ class userTest(unittest.TestCase):
             }
         )
         self.assertEqual(r.status_code, 201)
-
-    # GET request to /api/user returns the details of all users
-    def test_2_get_all_users(self):
-        r = requests.get("http://localhost:5000/api/user")
-        self.assertEqual(r.status_code, 200)
-
-    # GET request to /api/user returns the details of a singme user
-    def test_3_get_single_user(self):
-        r = requests.get("http://localhost:5000/api/user", params={id : 1})
-        self.assertEqual(r.status_code, 200)
